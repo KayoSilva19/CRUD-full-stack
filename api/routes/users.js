5e9b80c9cb61ed7d6d9269f952f1714b753efcc2
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-11-18 23:55:03 -0300

    rotes api

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getUsers } from "../controllers/user.js";
+import { addUsers, deleteUser, getUsers, updateUser } from "../controllers/user.js";
 
 const router = express.Router()
 
 router.get("/", getUsers)
+router.post("/", addUsers)
+
+
+router.put("/:id", updateUser)
+
+router.delete("/:id", deleteUser)
 
 export default router
